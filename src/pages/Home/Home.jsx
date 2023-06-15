dbe993b6ca4f35abb6c72d93c036968f06d5183e
khaoula <khaoulakhemiri20@gmail.com>
2023-06-15 14:57:21 +0100

    api test

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,8 @@
-import{ React,useState,useEffect,useMemo} from 'react';
+import { React, useState, useEffect, useMemo } from 'react';
 import Chart from "../../components/chart/Chart";
 import Feautredinfo from "../../components/feautredinfo/Feautredinfo";
 import "./home.css";
-import {userRequest} from "../../requestMethodes"
+import { userRequest } from "../../requestMethodes"
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
 
@@ -30,9 +30,12 @@ const Home = () => {
   useEffect(() => {
     const getStats = async () => {
       try {
-        const res = await userRequest.get("/users/stats");
+        const res = await axios.create({
+          baseURL: BASE_URL,
+          headers: { token: `Bearer ${currentUser?.accessToken}` }
+        }).get("/users/stats");
         console.log(res.data);
-        const data=res.data.sort((a, b) => a._id - b._id)
+        const data = res.data.sort((a, b) => a._id - b._id)
         console.log(data);
         res.data.map((item) =>
           setUserStats((prev) => [
@@ -40,15 +43,15 @@ const Home = () => {
             { name: MONTHS[item._id - 1], "New User": item.total },//take month number in the array
           ])
         );
-      }  catch (err) {
+      } catch (err) {
         console.log(err.response.data);
         console.log("err")
-    }
+      }
     };
     getStats();
-    
+
   }, [MONTHS]);
-  
+
   return (
     <div className="home">
       <Feautredinfo />
